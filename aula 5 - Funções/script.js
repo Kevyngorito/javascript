8dc37202b7f4192fc7cf2d6162a05fcd2cc3d596
Kevyn Gorito <kevyndccap@gmail.com>
2022-03-22 15:03:51 -0300

    Conhecendo funções em JavaScript

---
+++ aula 5 - Funções/script.js
@@ -0,0 +1,12 @@
+// Trabalhando com functions
+function funcao () {
+    console.log ('Tudo certo jovem gafanhoto!');
+}
+    funcao();
+// () indica que é um objeto do tipo function;
+// {} indica que é um bloco de instrução;]
+
+function soma (n1,n2) {
+    console.log (n1+n2)
+}
+    soma (10,20);
